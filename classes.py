8628c06306c1c6f89c10235bee08a3bbb4693a59
Archarios <archarios0ed@gmail.com>
2026-06-26 23:43:42 +0900

    envの実装を少し更新、オブジェクト内でselfが適切に使用されていなかったのでそこを修正

--- classes.py
+++ classes.py
@@ -18,7 +18,7 @@ class Value:
 class Continuation:
     pass
 
-@dataclass(frozen=True)#SICPの3.2を参考に実装
+@dataclass(frozen=True)#SICPの3.2、4.1.3を参考に実装
 class Env:
     def __init__(self:Env, enclosing_env : Env = None):
         self.frame = {}
@@ -39,19 +39,19 @@ class Env:
                 raise ValueError("The number of variables is greater than the number of values.")
         new_env = Env(self)
         for v, v_val in zip(var, val):
-            define_variable(v, v_val, new_env)
+            new_env.define_variable(v, v_val)
         return new_env
 
-    def define_variable(var : Var, val : Value, env : Env) -> None:
+    def define_variable(self, var : Var, val : Value) -> None:
         # adds to the first frame in the environment ⟨env ⟩ a new binding that associates the variable ⟨var⟩ with the value ⟨value ⟩
-        env.frame[var] = val
+        self.frame[var] = val
 
-    def set_variable_value(var : Var, val : Value, env : Env) -> None:
+    def set_variable_value(self, var : Var, val : Value) -> None:
         #changes the binding of the variable ⟨var⟩in the environment⟨env ⟩ so that the variable is now bound to the value ⟨value ⟩, or signals an error if the variable is unbound.
-        if var in env.frame:
-            env.frame[var] = val
-        elif env.enclosing_env is not None:
-            env.enclosing_env.set_variable_value(var, val, env)
+        if var in self.frame:
+            self.frame[var] = val
+        elif self.enclosing_env is not None:
+            self.enclosing_env.set_variable_value(var, val)
 
 @dataclass(frozen=True)
 class Var:
