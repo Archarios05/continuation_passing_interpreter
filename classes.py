8f3a59bd84fc688aa4f55445563a873e699d4032
Archarios <archarios0ed@gmail.com>
2026-06-26 23:47:28 +0900

    環境の定義を別ファイルに移動した

--- classes.py
+++ classes.py
@@ -18,40 +18,6 @@ class Value:
 class Continuation:
     pass
 
-@dataclass(frozen=True)#SICPの3.2、4.1.3を参考に実装
-class Env:
-    def __init__(self:Env, enclosing_env : Env = None):
-        self.frame = {}
-        self.enclosing_env = enclosing_env
-    def lookup_variable_value(self, var : Var) -> Value:
-        #環境でvarに束縛されている値を返す、束縛されていない場合はエラー
-        if  var in self.frame:
-            return self.frame[var]
-        elif self.enclosing_env is not None:
-            return self.enclosing_env.lookup_variable_value(var)
-
-    def extend_env(self, var : Var, val : Value) -> Env: 
-        # returns a new environment, consisting of a new frame in whichthe symbols in the list ⟨variables⟩ are bound to the corresponding elements in the list ⟨values⟩, where the enclosing environment is the environment ⟨base-env ⟩
-        if len(var) != len(val):
-            if len(var)< len(val):
-                raise ValueError("The number of variables is less than the number of values.")
-            else:
-                raise ValueError("The number of variables is greater than the number of values.")
-        new_env = Env(self)
-        for v, v_val in zip(var, val):
-            new_env.define_variable(v, v_val)
-        return new_env
-
-    def define_variable(self, var : Var, val : Value) -> None:
-        # adds to the first frame in the environment ⟨env ⟩ a new binding that associates the variable ⟨var⟩ with the value ⟨value ⟩
-        self.frame[var] = val
-
-    def set_variable_value(self, var : Var, val : Value) -> None:
-        #changes the binding of the variable ⟨var⟩in the environment⟨env ⟩ so that the variable is now bound to the value ⟨value ⟩, or signals an error if the variable is unbound.
-        if var in self.frame:
-            self.frame[var] = val
-        elif self.enclosing_env is not None:
-            self.enclosing_env.set_variable_value(var, val)
 
 @dataclass(frozen=True)
 class Var:
--- env_representation.py
+++ env_representation.py
@@ -1,21 +1,40 @@
-"EPL p38"
-"フレームで環境を管理する"
+import sys
+import os
+from __future__ import annotations
+from classes import *
 
-from types import Var, Value
-from types import Env
 
+@dataclass(frozen=True)#SICPの3.2、4.1.3を参考に実装
+class Env:
+    def __init__(self:Env, enclosing_env : Env = None):
+        self.frame = {}
+        self.enclosing_env = enclosing_env
+    def lookup_variable_value(self, var : Var) -> Value:
+        #環境でvarに束縛されている値を返す、束縛されていない場合はエラー
+        if  var in self.frame:
+            return self.frame[var]
+        elif self.enclosing_env is not None:
+            return self.enclosing_env.lookup_variable_value(var)
 
-def lookup_variable_value(var : Var, env : Env) -> Value:
-    #returns the value that is bound to the symbol ⟨var⟩ in the environment ⟨env ⟩, or signals an error if the variable is unbound.
-    pass
+    def extend_env(self, var : Var, val : Value) -> Env: 
+        # returns a new environment, consisting of a new frame in whichthe symbols in the list ⟨variables⟩ are bound to the corresponding elements in the list ⟨values⟩, where the enclosing environment is the environment ⟨base-env ⟩
+        if len(var) != len(val):
+            if len(var)< len(val):
+                raise ValueError("The number of variables is less than the number of values.")
+            else:
+                raise ValueError("The number of variables is greater than the number of values.")
+        new_env = Env(self)
+        for v, v_val in zip(var, val):
+            new_env.define_variable(v, v_val)
+        return new_env
 
-def extend_env(var : Var, val : Value, env : Env) -> Env: 
-    # returns a new environment, consisting of a new frame in whichthe symbols in the list ⟨variables⟩ are bound to the corresponding elements in the list ⟨values⟩, where the enclosing environment is the environment ⟨base-env ⟩
-    pass
+    def define_variable(self, var : Var, val : Value) -> None:
+        # adds to the first frame in the environment ⟨env ⟩ a new binding that associates the variable ⟨var⟩ with the value ⟨value ⟩
+        self.frame[var] = val
 
-def define_variable(var : Var, val : Value, env : Env) -> None:
-    # adds to the first frame in the environment ⟨env ⟩ a new binding that associates the variable ⟨var⟩ with the value ⟨value ⟩
-    pass
-
-def set_variable_value(var : Var, val : Value, env : Env) -> None:
-    #changes the binding of the variable ⟨var⟩in the environment⟨env ⟩ so that the variable is now bound to the value ⟨value ⟩, or signals an error if the variable is unbound.
+    def set_variable_value(self, var : Var, val : Value) -> None:
+        #changes the binding of the variable ⟨var⟩in the environment⟨env ⟩ so that the variable is now bound to the value ⟨value ⟩, or signals an error if the variable is unbound.
+        if var in self.frame:
+            self.frame[var] = val
+        elif self.enclosing_env is not None:
+            self.enclosing_env.set_variable_value(var, val)
